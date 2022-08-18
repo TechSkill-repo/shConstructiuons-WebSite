dfb2924efb6139a9f23d4787757ee069b5a30cd2
Fahad Mahmood <79140036+CodeSwimmer-24@users.noreply.github.com>
2022-08-18 23:56:49 +0530

    App.js

--- sh-web/src/App.js
+++ sh-web/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Carousel from "react-bootstrap/Carousel";
 
 function App() {
   return (
     <div className="App">
-      <h1>SH Constructions</h1>
+      <h1>Hello</h1>
     </div>
   );
 }
