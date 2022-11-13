4dc7d0acba55cb2b24fd52597aab7021a2bbf22c
Mafiz <mafizonly@gmail.com>
2022-11-14 00:56:04 +0530

    date added to csr page

--- sh-web/src/components/Csr/Container/Container.js
+++ sh-web/src/components/Csr/Container/Container.js
@@ -1,124 +1,180 @@
-import './Container.scss'
+import "./Container.scss";
 
 function Container() {
+  const fakeUrls = [
+    "https://source.unsplash.com/?fruit",
+    "https://source.unsplash.com/?cars",
+    "https://source.unsplash.com/?construction",
+    "https://source.unsplash.com/?nature",
+    "https://source.unsplash.com/?city,night",
+    "https://source.unsplash.com/?city,day",
+    "https://source.unsplash.com/?people",
+    "https://source.unsplash.com/?countries",
+    "https://source.unsplash.com/random/?city,night",
+    "https://source.unsplash.com/random",
+    "https://source.unsplash.com/random",
+    "https://source.unsplash.com/random",
+    "https://source.unsplash.com/random",
+    "https://source.unsplash.com/random",
+    "https://source.unsplash.com/random",
+    "https://source.unsplash.com/random",
+    "https://source.unsplash.com/random",
+    "https://source.unsplash.com/random",
+    "https://source.unsplash.com/random",
+    "https://source.unsplash.com/random",
+    "https://source.unsplash.com/random",
+    "https://source.unsplash.com/random",
+  ];
 
-    const fakeUrls =[
-        "https://source.unsplash.com/?fruit",
-        "https://source.unsplash.com/?cars",
-        "https://source.unsplash.com/?construction",
-        "https://source.unsplash.com/?nature",
-        "https://source.unsplash.com/?city,night",
-        "https://source.unsplash.com/?city,day",
-        "https://source.unsplash.com/?people",
-        "https://source.unsplash.com/?countries",
-        "https://source.unsplash.com/random/?city,night",
-        "https://source.unsplash.com/random",
-        "https://source.unsplash.com/random",
-        "https://source.unsplash.com/random",
-        "https://source.unsplash.com/random",
-        "https://source.unsplash.com/random",
-        "https://source.unsplash.com/random",
-        "https://source.unsplash.com/random",
-        "https://source.unsplash.com/random",
-        "https://source.unsplash.com/random",
-        "https://source.unsplash.com/random",
-        "https://source.unsplash.com/random",
-        "https://source.unsplash.com/random",
-        "https://source.unsplash.com/random",
+  return (
+    <div className="csr-container">
+      <div className="heading">
+        <p className="clip">HOW WE HELP</p>
+        <h1>Corporate Social Responsibility</h1>
+      </div>
 
-    ]
-
-
-
-    return (
-    <div className='csr-container'>
-
-
-        <div className="heading">
-            <p className="clip">HOW WE HELP</p>
-            <h1>Corporate Social Responsibility</h1>
-
-        </div>
-
-        <div className="csr-images">
-
-            <div className="csr-row">
-
-        <div className="csr-image">
-
-            
-                    <img src="https://okcredit-blog-images-prod.storage.googleapis.com/2021/01/socialservice3.jpg" alt="" />
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi dignissimos consequatur ratione iste asperiores consectetur?</p>
-                </div>
-                <div className="csr-image">
-                    <img src="https://images.shiksha.com/mediadata/images/articles/1624866875phpZq9RM9.jpeg" alt="" />
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi dignissimos consequatur ratione iste asperiores consectetur?</p>
-                </div>
-       
-                </div>
-
-                <div className="csr-row csr-row-more">
-
-<div className="csr-image">
-
-    
-            <img src="http://im.rediff.com/getahead/2013/jul/19socialwork4.jpg" alt="" />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi dignissimos consequatur ratione iste asperiores consectetur?</p>
-        </div>
-        <div className="csr-image">
-
-            
-                    <img src="https://okcredit-blog-images-prod.storage.googleapis.com/2021/01/socialservice3.jpg" alt="" />
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi dignissimos consequatur ratione iste asperiores consectetur?</p>
-                </div>
-        <div className="csr-image">
-            <img src="https://socialwork.buffalo.edu/content/socialwork/home/admissions/is-social-work-right-career-for-me/list-of-essential-skills-in-social-work/_jcr_content/par/image.img.680.auto.png/1529780679625.png" alt="" />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi dignissimos consequatur ratione iste asperiores consectetur?</p>
-        </div>
-
-        </div>
-
+      <div className="csr-images">
         <div className="csr-row">
-
-<div className="csr-image">
-
-    
-            <img src="https://okcredit-blog-images-prod.storage.googleapis.com/2021/01/socialservice3.jpg" alt="" />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi dignissimos consequatur ratione iste asperiores consectetur?</p>
-        </div>
-        <div className="csr-image">
-            <img src="https://images.shiksha.com/mediadata/images/articles/1624866875phpZq9RM9.jpeg" alt="" />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi dignissimos consequatur ratione iste asperiores consectetur?</p>
-        </div>
-
+          <div className="csr-image">
+            <img
+              src="https://okcredit-blog-images-prod.storage.googleapis.com/2021/01/socialservice3.jpg"
+              alt=""
+            />
+            <div>
+              <p className="date">{new Date().toLocaleDateString()}</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+                dignissimos consequatur ratione iste asperiores consectetur?
+              </p>
+            </div>
+          </div>
+          <div className="csr-image">
+            <img
+              src="https://images.shiksha.com/mediadata/images/articles/1624866875phpZq9RM9.jpeg"
+              alt=""
+            />
+            <div>
+              <p className="date">{new Date().toLocaleDateString()}</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+                dignissimos consequatur ratione iste asperiores consectetur?
+              </p>
+            </div>
+          </div>
         </div>
 
         <div className="csr-row csr-row-more">
-        <div className="csr-image">
-            <img src="https://socialwork.buffalo.edu/content/socialwork/home/admissions/is-social-work-right-career-for-me/list-of-essential-skills-in-social-work/_jcr_content/par/image.img.680.auto.png/1529780679625.png" alt="" />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi dignissimos consequatur ratione iste asperiores consectetur?</p>
+          <div className="csr-image">
+            <img
+              src="http://im.rediff.com/getahead/2013/jul/19socialwork4.jpg"
+              alt=""
+            />
+            <div>
+              <p className="date">{new Date().toLocaleDateString()}</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+                dignissimos consequatur ratione iste asperiores consectetur?
+              </p>
+            </div>
+          </div>
+          <div className="csr-image">
+            <img
+              src="https://okcredit-blog-images-prod.storage.googleapis.com/2021/01/socialservice3.jpg"
+              alt=""
+            />
+             <div>
+
+<p className="date">{new Date().toLocaleDateString()}</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi dignissimos consequatur ratione iste asperiores consectetur?</p>
+</div>
+          </div>
+          <div className="csr-image">
+            <img
+              src="https://socialwork.buffalo.edu/content/socialwork/home/admissions/is-social-work-right-career-for-me/list-of-essential-skills-in-social-work/_jcr_content/par/image.img.680.auto.png/1529780679625.png"
+              alt=""
+            />
+             <div>
+
+<p className="date">{new Date().toLocaleDateString()}</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi dignissimos consequatur ratione iste asperiores consectetur?</p>
+</div>
+          </div>
         </div>
 
-<div className="csr-image">
-
-          
-            <img src="https://okcredit-blog-images-prod.storage.googleapis.com/2021/01/socialservice3.jpg" alt="" />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi dignissimos consequatur ratione iste asperiores consectetur?</p>
+        <div className="csr-row">
+          <div className="csr-image">
+            <img
+              src="https://okcredit-blog-images-prod.storage.googleapis.com/2021/01/socialservice3.jpg"
+              alt=""
+            />
+            <div>
+              <p className="date">{new Date().toLocaleDateString()}</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+                dignissimos consequatur ratione iste asperiores consectetur?
+              </p>
+            </div>
+          </div>
+          <div className="csr-image">
+            <img
+              src="https://images.shiksha.com/mediadata/images/articles/1624866875phpZq9RM9.jpeg"
+              alt=""
+            />
+            <div>
+              <p className="date">{new Date().toLocaleDateString()}</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+                dignissimos consequatur ratione iste asperiores consectetur?
+              </p>
+            </div>
+          </div>
         </div>
-        <div className="csr-image">
-            <img src="https://images.shiksha.com/mediadata/images/articles/1624866875phpZq9RM9.jpeg" alt="" />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi dignissimos consequatur ratione iste asperiores consectetur?</p>
+
+        <div className="csr-row csr-row-more">
+          <div className="csr-image">
+            <img
+              src="https://socialwork.buffalo.edu/content/socialwork/home/admissions/is-social-work-right-career-for-me/list-of-essential-skills-in-social-work/_jcr_content/par/image.img.680.auto.png/1529780679625.png"
+              alt=""
+            />
+            <div>
+              <p className="date">{new Date().toLocaleDateString()}</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+                dignissimos consequatur ratione iste asperiores consectetur?
+              </p>
+            </div>
+          </div>
+
+          <div className="csr-image">
+            <img
+              src="https://okcredit-blog-images-prod.storage.googleapis.com/2021/01/socialservice3.jpg"
+              alt=""
+            />
+            <div>
+              <p className="date">{new Date().toLocaleDateString()}</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+                dignissimos consequatur ratione iste asperiores consectetur?
+              </p>
+            </div>
+          </div>
+          <div className="csr-image">
+            <img
+              src="https://images.shiksha.com/mediadata/images/articles/1624866875phpZq9RM9.jpeg"
+              alt=""
+            />
+            <div>
+              <p className="date">{new Date().toLocaleDateString()}</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+                dignissimos consequatur ratione iste asperiores consectetur?
+              </p>
+            </div>
+          </div>
         </div>
-
-        </div>
-
-        
-  
-
-        </div>
-
+      </div>
     </div>
-  )
+  );
 }
 
-export default Container
+export default Container;
