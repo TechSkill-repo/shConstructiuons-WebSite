d26dd3c094052892cc80b68387f8d8d7b465bd52
Mafiz <mafizonly@gmail.com>
2022-11-18 15:39:37 +0530

    yearly event page done

--- sh-web/src/components/Events/Container/Container.js
+++ sh-web/src/components/Events/Container/Container.js
@@ -1,5 +1,11 @@
 import "./Container.scss";
 import Picture1 from "../../../assets/Picture1.jpg";
+import yearly1 from "../../../assets/yearly1.png";
+import yearly2 from "../../../assets/yearly2.jpg";
+import yearly3 from "../../../assets/yearly3.png";
+import yearly4 from "../../../assets/yearly4.jpg";
+import yearly5 from "../../../assets/yearly5.jpg";
+import yearly6 from "../../../assets/yearly6.jpg";
 
 function Container() {
   return (
@@ -27,9 +33,9 @@ function Container() {
       <div className="events-main">
         <div className="events-main-heading">
           <h1>Opening Speech</h1>
-          <p className="clip">Mr. Tahir Khan (Site in-charge SHC)</p>
-        
-          <p className="clip">Location – RMBB</p>
+          <p className="clip event-clip">Mr. Tahir Khan (Site in-charge SHC)</p>
+
+          <p className="clip event-clip">Location – RMBB</p>
         </div>
         <div className="events-main-contents">
           <div className="events-main-contents-left">
@@ -80,6 +86,109 @@ function Container() {
           </div>
         </div>
       </div>
+
+      <div className="events-main">
+        <div className="events-main-heading">
+          <h3>SAFETY PAUSE AND ADDRESSING EVENT</h3>
+          <ul>
+            <li>
+              <p className="para">
+                Safety Pause by Site Safety Supervisor Mr. Abhishek Shared the
+                Incident and Lesson Learned with Crew
+              </p>
+            </li>
+            <li>
+              <p className="para">
+                Mr. Tahir Khan (Site In-charge) addressed the employees on
+                safety precautions during rain, behavior safety, CSM
+                requirement, Corona Booster dose and skill upgradation and asked
+                everyone to work safely and stay safe during rain
+              </p>
+            </li>
+          </ul>
+        </div>
+        <div className="events-main-contents container2">
+          <div className="events-main-contents-left">
+            <img src={yearly1} alt="" />
+          </div>
+          <div className="events-main-contents-right">
+            <img src={yearly3} alt="" />
+          </div>
+        </div>
+
+        <div className="events-main-contents container2">
+          <div className="events-main-contents-left">
+            <h3>
+              TWO WAY COMMUNICATION AND INTERACTION BETWEEN TSL REPRESENTATIVE,
+              SHC REPRESENTATIVE AND WORKFORCE
+            </h3>
+            <p className="para">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam
+              sint ipsa error nostrum quam laudantium asperiores quidem.
+              Dolores, fuga error?Lorem ipsum, dolor sit amet consectetur
+              adipisicing elit. Explicabo, fugiat. Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Possimus eligendi officia vitae
+              sequi in. Fugit, obcaecati! Nobis praesentium et repellendus.
+            </p>
+          </div>
+          <div className="events-main-contents-right">
+            <img src={yearly2} alt="" />
+          </div>
+        </div>
+      </div>
+
+      <div className="events-main">
+        <div className="events-main-heading">
+          <h3>QUIZ COMPETITON AND AWARD DISTRIBUTION</h3>
+          <p className="para">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+            iusto quisquam, dolorem eveniet a similique inventore expedita
+            officiis praesentium. Deleniti accusantium eos excepturi, odio
+            veniam aliquid nobis similique consequuntur iusto optio temporibus
+            qui, exercitationem alias veritatis dolor. Rem similique repellat
+            cum maiores voluptatibus facere quo perferendis. Voluptas magni ad
+            harum. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quisquam, quae.
+          </p>
+        </div>
+        <div className="events-main-contents container2">
+          <div className="events-main-contents-left qa">
+            <img src={yearly4} alt="" />
+            <p className="para">
+              Que:- An easy way to know if you are maintaining the safe distance
+              is to take ?
+              <br />
+              यह जानने का एक आसान तरीका है कि आप सुरक्षित दूरी बनाए रख रहे हैं ?
+              <br />
+              Ans:- “The two second rule” – Mr. Ganesh Kuntia
+            </p>
+          </div>
+          <div className="events-main-contents-right qa">
+            <img src={yearly5} alt="" />
+            <p className="para">
+              Que:- What is the length of safety harness lanyards?
+              <br />
+              सेफ्टी हार्नेस डोरी की लंबाई क्या है?
+              <br />
+              Ans:- “1.8 meter”–Mr. Shiv Charna Patro
+            </p>
+          </div>
+        </div>
+        
+      </div>
+
+      <div className="events-main reward">
+
+        <div className="events-main-heading reward-heading">
+          <h3>REWARD AND RECOGNITION</h3>
+          <p className="para">
+          Reward and Recognition: Employee of the Month- Mr. Rajesh Rana for excellent performance in the job
+          </p>
+        </div>
+         
+           <img src={yearly6} alt="" />
+           
+        </div>
     </div>
   );
 }
--- sh-web/src/pages/Events/Events.js
+++ sh-web/src/pages/Events/Events.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Container from '../../components/Events/Container/Container'
 import Hero from '../../components/Events/Hero/Hero'
+import Footer from '../../components/Footer/Footer'
 
 function Events({type}) {
   return (
     <>
        <Hero type={type}></Hero>
        <Container></Container>
+       <Footer></Footer>
     
     </>
   )
