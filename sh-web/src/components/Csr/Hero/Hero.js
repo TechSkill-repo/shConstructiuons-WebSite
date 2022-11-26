ee65b19b2ee1f21b82397401ed7a3795ae337911
Fahad Mahmood <79140036+CodeSwimmer-24@users.noreply.github.com>
2022-11-26 15:11:09 +0530

    Chsnge

--- sh-web/src/components/Csr/Hero/Hero.js
+++ sh-web/src/components/Csr/Hero/Hero.js
@@ -1,35 +1,29 @@
-import React from 'react'
-import Navbar from '../../Navbar/Navbar';
-import './Hero.scss';
-import heroBg from '../../../assets/about-page-hero.jpg'
-
+import React from "react";
+import Navbar from "../../Navbar/Navbar";
+import "./Hero.scss";
+import heroBg from "../../../assets/about-page-hero.jpg";
 
 function Hero() {
   return (
-   <div className="csr-page">
-            <Navbar></Navbar>
-        <img className="csr-hero-bg" src={"https://images.unsplash.com/photo-1582213782179-e0d53f98f2ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"} alt="" />
-    <div className="csr-hero">
-
+    <div className="csr-page">
+      <Navbar></Navbar>
+      <img
+        className="csr-hero-bg"
+        src={"https://www.dagrofa.dk/wp-content/uploads/CSR-web-1920x800.gif"}
+        alt=""
+      />
+      <div className="csr-hero">
         <div className="csr-hero-content">
           <h4>
-            <span>
-            Home 
-
-            </span>
-            <span className='delimeter'></span> 
-            <span>
-
-            CSR
-            </span>
-            </h4>
+            <span>Home</span>
+            <span className="delimeter"></span>
+            <span>CSR</span>
+          </h4>
           <h1>CSR</h1>
         </div>
-
-    
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
